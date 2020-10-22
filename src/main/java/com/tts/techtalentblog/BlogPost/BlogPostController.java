6cba5e40d7aa106c7f08c6912d73d38de5dfd68c
Ricardo P <ricardopaucar1363@gmail.com>
2020-10-22 00:06:32 -0500

    add changes for the new.html page

--- src/main/java/com/tts/techtalentblog/BlogPost/BlogPostController.java
+++ src/main/java/com/tts/techtalentblog/BlogPost/BlogPostController.java
@@ -1,5 +1,8 @@
 package com.tts.techtalentblog.BlogPost;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,19 +15,34 @@ public class BlogPostController {
     @Autowired
     private BlogPostRepository blogPostRepository;
 
+    private static List<BlogPost> posts = new ArrayList<>();
+
+    //index Page
     @GetMapping(value = "/")
-    public String index (BlogPost blogPost) {
+    public String index (BlogPost blogPost, Model model) {
         
+        model.addAttribute("posts", posts);
+
         // the return needs to be same as the folder name 
         // in the templates folder
         return "blogpost/index";
     }
 
+    // new Page
+    @GetMapping(value = "/blogpost/new")
+    public String index (BlogPost blogPost) {
+
+        // the return needs to be same as the folder name 
+        // in the templates folder
+        return "blogpost/new";
+    }
+
     private BlogPost blogPost;
 
-    @PostMapping(value = "/")
+    @PostMapping(value = "/blogposts")
     public String addNewBlogPost(BlogPost blogPost, Model model) {
-        blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+        blogPostRepository.save(blogPost);
+        posts.add(blogPost);
         model.addAttribute("title", blogPost.getTitle());
 	    model.addAttribute("author", blogPost.getAuthor());
 	    model.addAttribute("blogEntry", blogPost.getBlogEntry());
