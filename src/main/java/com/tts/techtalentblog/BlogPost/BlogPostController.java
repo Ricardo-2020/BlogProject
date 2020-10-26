51179456fc9c1706a074215318ada944c875f778
Ricardo P <ricardopaucar1363@gmail.com>
2020-10-26 01:22:09 -0500

    set up delete

--- src/main/java/com/tts/techtalentblog/BlogPost/BlogPostController.java
+++ src/main/java/com/tts/techtalentblog/BlogPost/BlogPostController.java
@@ -7,7 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class BlogPostController {
@@ -20,6 +25,10 @@ public class BlogPostController {
     //index Page
     @GetMapping(value = "/")
     public String index (BlogPost blogPost, Model model) {
+        posts.removeAll(posts);
+        for (BlogPost postFromDB : blogPostRepository.findAll()) {
+            posts.add(postFromDB);
+        }
         
         model.addAttribute("posts", posts);
 
@@ -42,12 +51,19 @@ public class BlogPostController {
     @PostMapping(value = "/blogposts")
     public String addNewBlogPost(BlogPost blogPost, Model model) {
         blogPostRepository.save(blogPost);
-        posts.add(blogPost);
+        //posts.add(blogPost);
         model.addAttribute("title", blogPost.getTitle());
 	    model.addAttribute("author", blogPost.getAuthor());
 	    model.addAttribute("blogEntry", blogPost.getBlogEntry());
 	    return "blogpost/result";
     }
+
+    @RequestMapping(value="/blogpost/{id}")
+    public String deletePostWithId(@PathVariable Long id, BlogPost blogPost, Model model) {
+        blogPostRepository.deleteById(id);
+        return "redirect:/";
+    }
+    
 
 
 
